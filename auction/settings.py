6632ed80dd0a10c013bd78c2241d949c88124cd4
SST-Bappu <bappu.ctg@gmail.com>
2021-12-14 23:45:21 +0600

    ...

--- auction/settings.py
+++ auction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'tw3-_l2&#(%*=kkfuaq8si=h4#oln&qxmvk0rp90!u$s+*^3#+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sattasundarauctions.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','sstauctions.herokuapp.com']
 
 
 # Application definition
